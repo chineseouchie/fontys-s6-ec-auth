f30380a60ed227c927efe835af038777a5169707
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-15 14:07:08 +0100

    Cors & body parser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-import express from 'express'
-import 'dotenv/config'
-import routes from './routes'
+import express from "express"
+import "dotenv/config"
+import routes from "./routes"
+import cors from "cors"
+import bodyParser from "body-parser"
 
 export default function(database) {
 	const app = express()
+	app.use(cors())
+	app.use(bodyParser.json())
 	app.use(express.json())
 	app.use(routes(database))
 
