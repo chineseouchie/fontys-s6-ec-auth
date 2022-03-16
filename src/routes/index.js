dc6cd2f68d0025dd62d8e6e480a846890184cce5
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-16 21:25:24 +0100

    Separated validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,25 @@
 import { Router } from "express";
 import login from "../controllers/login";
 import { register } from "../controllers/register"
-import { validateRegister } from "../validations/register";
+import { validateEmail, validatePassword, validateUser } from "../validations/register";
 
 export default (database) => {
 	const app = Router()
 
-	app.post("/api/v1/register", validateRegister, register(database))
-	app.post("/api/v1/login", login(database))
+	app.post(
+		"/api/v1/register",
+		validateEmail,
+		validatePassword, 
+		validateUser, 
+		register(database)
+	)
+
+	app.post(
+		"/api/v1/login",
+		validateEmail,
+		validatePassword,
+		login(database)
+	)
 	
 	return app
 }
--- src/validations/register.js
+++ src/validations/register.js
@@ -1,19 +1,12 @@
 import { json400 } from "../response/json"
 import { isValidEmail, isValidPassword } from "../utils/utils"
 
-export function validateRegister(req, res, next) {
-	const {email, password, firstname, lastname} = req.body
+export function validateEmail(req, res, next) {
+	const {email} = req.body
 	if (!email) {
 		return json400(
 			res,
 			"Missing email",
-		)
-	}
-
-	if (!password) {
-		return json400(
-			res,
-			"Missing password",
 		)
 	}
 
@@ -23,6 +16,17 @@ export function validateRegister(req, res, next) {
 			"Invalid email"
 		)
 	}
+	next()
+}
+
+export function validatePassword(req, res, next) {
+	const {password} = req.body
+	if (!password) {
+		return json400(
+			res,
+			"Missing password",
+		)
+	}	
 
 	if (!isValidPassword(password)) {
 		return json400(
@@ -30,7 +34,11 @@ export function validateRegister(req, res, next) {
 			"Invalid password"
 		)
 	}
+	next()
+}
 
+export function validateUser(req, res, next) {
+	const {firstname, lastname} = req.body
 	if (!firstname) {
 		return json400(
 			res,
@@ -44,6 +52,5 @@ export function validateRegister(req, res, next) {
 			"Missing lastname"
 		)
 	}
-
 	next()
 }
