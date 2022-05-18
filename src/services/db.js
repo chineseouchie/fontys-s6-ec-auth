94eb487c8fb50255ad5871e3f780e3c3ad643040
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-05-18 11:41:12 +0200

    Get roles from auth

--- src/services/db.js
+++ src/services/db.js
@@ -29,14 +29,25 @@ export async function createNewAccount(email, password, uuid) {
 		let roleSql = ""
 		if (email == "admin@example.com") {
 			roleSql = `SELECT * FROM role WHERE name = "ADMIN"`;
+			const [role1] = await conn.query(roleSql)
+		
+			// Create user role 
+			const userRole1 = `INSERT INTO user_role(user_id, role_id) VALUES(${rows.insertId},${role1[0].role_id})`
+			await conn.query(userRole1)
+
+			roleSql = `SELECT * FROM role WHERE name = "EMPLOYEE"`;
+			const [role2] = await conn.query(roleSql)
+		
+			// Create user role 
+			const userRole2 = `INSERT INTO user_role(user_id, role_id) VALUES(${rows.insertId},${role2[0].role_id})`
+			await conn.query(userRole2)
 		} else {
 			roleSql = `SELECT * FROM role WHERE name = "USER"`;
+			const [role] = await conn.query(roleSql)
+			const userRole = `INSERT INTO user_role(user_id, role_id) VALUES(${rows.insertId},${role[0].role_id})`
+			await conn.query(userRole)
 		}
-		const [role] = await conn.query(roleSql)
 		
-		// Create user role 
-		const userRole = `INSERT INTO user_role(user_id, role_id) VALUES(${rows.insertId},${role[0].role_id})`
-		await conn.query(userRole)
 
 		await conn.commit()
 		return [null]
@@ -70,6 +81,26 @@ export async function findOneAuthByEmail(email) {
 
 		if (rows.length >= 1 ){
 			return [rows[0], null]
+		} else {
+			return [null, null]
+
+		}
+	} catch (e) {
+		console.error(e)
+		return [null, e]
+	}
+}
+
+export async function getRolesFromUser(auth_id) {
+	try {
+		const sql = `SELECT r.name, r.role_id FROM role as r
+		INNER JOIN user_role AS ur ON r.role_id = ur.role_id
+		WHERE ur.USER_ID = ?;`
+
+		const [rows,] = await pool.query(sql, [auth_id])
+		
+		if (rows.length >= 1 ){
+			return [rows, null]
 		} else {
 			return [null, null]
 
