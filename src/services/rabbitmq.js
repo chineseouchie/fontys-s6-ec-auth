d8903fec5a159379f5f570a3544850e8be3a02f5
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-05-18 09:57:41 +0200

    Added reconnect attempts

--- src/services/rabbitmq.js
+++ src/services/rabbitmq.js
@@ -1,16 +1,23 @@
 import amqp from "amqplib"
 import jsonwebtoken from "jsonwebtoken"
 import "dotenv/config"
+import * as Sentry from "@sentry/node";
+
+// Importing @sentry/tracing patches the global hub for tracing to work.
+import "@sentry/tracing";
 
 const host = process.env.RABBITMQ_HOST
 const user = process.env.RABBITMQ_USER
 const password = process.env.RABBITMQ_PASSWORD 
 let channel
+let attempt = 0
 
 export async function init() {
 	console.log("Connecting with RabbitMQ...")
 	try {
 		const conn = await amqp.connect(`amqp://${user}:${password}@${host}:5672`)
+
+		// conn.on("error", (err)=>{console.log(err, "failed")})
 		channel = await conn.createChannel()
 		console.log("Connected with RabbitMQ")
 
@@ -19,6 +26,21 @@ export async function init() {
 	} catch(e) {
 		console.log("Failed to connect with RabbitMQ")
 		console.log(e)
+		setTimeout(() => {
+			attempt += 1
+			if (attempt < 5) {
+				console.log(`Trying to reconnect with RabbitMQ. Attempt: #${attempt}`)
+				init()
+			} else {
+				const transaction = Sentry.startTransaction({
+					op: "RabbitMQ connection failed",
+					name: "ec-auth <--> rabbitmq",
+				});
+				Sentry.captureException(e)
+				transaction.finish()
+				console.log("Failed to reconnect with RabbitMQ.")				
+			}
+		}, 1000);
 	}
 }
 
